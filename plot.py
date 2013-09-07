af6bf34625f62bc69e86fc39bccb847e6fa9afbe
rapt0r <alex.jce@gmail.com>
2013-09-07 18:25:51 +0300

    Generation changed

--- plot.py
+++ plot.py
@@ -7,14 +7,32 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument("file_in", type=str, help="NumPy array file", default="pointmap.npy")
 parser.add_argument("file_out", type=str, help="Output file name", default="fractal.png")
+parser.add_argument("size", type=int, help="Image size")
 args = parser.parse_args()
 
-points = np.load(args.file_in)
-size = len(points)
+size = args.size
+roots = np.load(args.file_in)
+hits = np.zeros((size, size), dtype=np.int)
 image = Image.new("RGB", (size, size))
 
-max_hits = np.log(np.amax(points))
-for xi, xd in enumerate(points):
+x_max = 1.5
+x_min = -1.5
+y_max = 1.5
+y_min = -1.5
+
+# Generate Hit map
+for root in roots:
+    point_x = (size - 1) * (root[0] - x_min) / (x_max - x_min)
+    point_y = (size - 1) * (root[1] - y_min) / (y_max - y_min)
+    if all([point_x >= 0,
+            point_x <= size - 1,
+            point_y >= 0,
+            point_y <= size - 1]):
+                hits[int(point_x)][int(point_y)] += 1
+
+# Plot hits on image
+max_hits = np.log(np.amax(hits))
+for xi, xd in enumerate(hits):
     for yi, yd in enumerate(xd):
         if yd > 0:
             c_val = np.log(yd) / max_hits
--- roots.py
+++ roots.py
@@ -6,30 +6,18 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("degree", type=int, help="Polynomial degree")
-parser.add_argument("size", type=int, help="Array size")
 parser.add_argument("filename", type=str, default="pointmap.npy")
 args = parser.parse_args()
 
 degree = args.degree
-size = args.size
-points = np.zeros((size, size), dtype=np.int)
-
-x_max = 1.5
-x_min = -1.5
-y_max = 1.5
-y_min = -1.5
+points = list()
 
 start_t = time()
 for poly in itertools.product(*([[-1, 1]] * degree)):
     for root in np.roots((1,) + poly):
-        point_x = (size - 1) * (root.real - x_min) / (x_max - x_min)
-        point_y = (size - 1) * (root.imag - y_min) / (y_max - y_min)
-        if all([point_x >= 0,
-                point_x <= size - 1,
-                point_y >= 0,
-                point_y <= size - 1]):
-                    points[int(point_x)][int(point_y)] += 1
+        points.append((root.real, root.imag))
 
+roots_data = np.array(points, dtype=np.float64)
 print "Complete in {} seconds.".format(str(int(time() - start_t)))
 with open(args.filename, "w") as out_file:
-    np.save(out_file, points)
+    np.save(out_file, roots_data)
